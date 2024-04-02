52d23cd98a6ded279016f694cabe59df43f9ef22
Mig-Garmor <thecodermiguel@gmail.com>
2024-04-02 18:09:11 +0200

    Websocket connection successful

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,32 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 import Group from "./sections/group/Group";
+import Pusher from "pusher-js";
+import Echo from "laravel-echo";
 
 // Create a client
 const queryClient = new QueryClient();
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+  broadcaster: "pusher",
+  cluster: "mt1",
+  key: import.meta.env.VITE_APP_PUSHER_APP_KEY,
+  wsHost: "localhost",
+  wsPort: 6001,
+  // wssport: import.meta.env.VITE_APP_PUSHER_APP_KEY,
+  transports: ["websocket"],
+  enabledTransports: ["ws", "wss"],
+  forceTLS: false,
+  encrypted: false,
+  disableStats: true,
+  logToConsole: true, // Enable logging for debugging
+});
+
+window.Echo.channel("message").listen("NewMessage", (event) => {
+  console.log(event);
+});
 
 function App() {
   return (
